4662b515cf69cfe7cb4dd9c5d98808d8c57a66e8
kpence <pencekyle@gmail.com>
2014-03-27 20:38:22 -0500

    Yes

--- config.h
+++ config.h
@@ -197,10 +197,10 @@ static Button buttons[] = {
 
 	{ ClkWinTitle,          0,              Button2,       untogglefloating,{0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkWinTitle,          MODKEY,         Button4,        setmfact,       {.f = +0.05} },
-	{ ClkWinTitle,          MODKEY,         Button5,        setmfact,       {.f = -0.05} },
-	{ ClkWinTitle,          MODKEY|ShiftMask,Button4,       setmfact,       {.f = +0.01} },
-	{ ClkWinTitle,          MODKEY|ShiftMask,Button5,       setmfact,       {.f = -0.01} },
+	{ ClkWinTitle,          MODKEY,         Button4,        setmfact,       {.f = -0.05} },
+	{ ClkWinTitle,          MODKEY,         Button5,        setmfact,       {.f = +0.05} },
+	{ ClkWinTitle,          MODKEY|ShiftMask,Button4,       setmfact,       {.f = -0.01} },
+	{ ClkWinTitle,          MODKEY|ShiftMask,Button5,       setmfact,       {.f = +0.01} },
 
 	{ ClkStatusText,        MODKEY,         Button2,        spawn,          {.v = termcmd } },
 
